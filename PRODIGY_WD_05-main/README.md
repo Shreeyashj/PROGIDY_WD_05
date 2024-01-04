3b4add4900270c4f67a8aae10ddf7cb782f662e0
Shreeyashj <129369982+Shreeyashj@users.noreply.github.com>
2024-01-04 16:30:37 +0530

    Add files via upload

---
+++ PRODIGY_WD_05-main/README.md
@@ -0,0 +1,55 @@
+# PRODIGY_WD_05
+
+This is my Task 05 of my Internship at Prodigy Infotech 
+
+# WeatherApp
+
+Welcome to WeatherApp – Your Stylish Weather Companion!
+
+## Overview
+
+WeatherApp is a sleek and intuitive web application designed to provide accurate and stylish weather forecasts. Whether you're planning a trip, checking the weather for a special event, or simply staying informed about your surroundings, WeatherApp has got you covered.
+
+## Features
+
+- **Stylish Design:** Immerse yourself in a visually stunning interface with a classy color palette that enhances your weather-checking experience.
+
+- **Location Intelligence:** Easily obtain weather details for any city by typing in the name. The app instantly fetches accurate data, making it convenient for users worldwide.
+
+- **Responsive Across Devices:** Enjoy a seamless experience on all devices, ensuring consistent functionality and design, whether you're on your desktop, tablet, or smartphone.
+
+- **Global Forecasting:** Explore weather conditions anywhere in the world. WeatherApp connects you with meteorological insights globally.
+
+- **Detailed Forecasts:** Beyond basics, get in-depth forecasts including temperature, wind speed, and humidity, empowering you to plan activities with confidence.
+
+## Getting Started
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/yourusername/WeatherApp.git
+
+2. **Navigate to the Project:**
+   ```bash
+   cd WeatherApp
+3. Open index.html in Your Browser:
+
+4. Double-click on the index.html file or open it using your preferred web browser.
+   Start Exploring:
+
+5. Explore the app, check the weather for your city, and experience the elegant design firsthand.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- [OpenWeatherMap API](https://openweathermap.org/api) for weather data
+
+## Acknowledgments
+
+   Special thanks to Youtube videos, Online resources and A.I tools   
+
+## 🌦️ **WeatherApp** – Bringing Elegance to Weather Forecasting 🌦️
+   Check it out: (https://jatush.github.io/PRODIGY_WD_04/)
+
+Connect with me on LinkedIn: (www.linkedin.com/in/jatush-hingu)
---
+++ PRODIGY_WD_05-main/script.js
@@ -0,0 +1,96 @@
+const API_KEY = "a422ba6a305c261cf34bee586042bff2";
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
+
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if (index === 0) {
+        return `<div class="details">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>"${weatherItem.weather[0].description}"</h6>
+                    <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                    <h6>Pressure: ${weatherItem.main.pressure}%</h6>
+                </div>`
+    } else {
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <h6>"${weatherItem.weather[0].description}"</h6>
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                    <h6>Pressure: ${weatherItem.main.pressure}%</h6>
+                </li>`;
+    }
+}
+
+const getWeatherDetails = (cityName, latitude, longitude) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
+            }
+        });
+
+        cityInput.value = "";
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = "";
+
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            const html = createWeatherCard(cityName, weatherItem, index);
+            if (index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", html);
+            }
+        });
+    }).catch(() => {
+        alert("Cannot fetch the weather forecast!, please reload the site");
+    });
+}
+
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if (cityName === "") return;
+    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+    fetch(API_URL).then(response => response.json()).then(data => {
+        if (!data.length) return alert(`No city found for ${cityName}, please retry`);
+        const { lat, lon, name } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("Can't get the coordinates!, Please retry");
+    });
+}
+
+const getUserCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            const { latitude, longitude } = position.coords;
+            const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            fetch(API_URL).then(response => response.json()).then(data => {
+                const { name } = data[0];
+                getWeatherDetails(name, latitude, longitude);
+            }).catch(() => {
+                alert("Can't get the city name, please retry");
+            });
+        },
+        error => {
+            if (error.code === error.PERMISSION_DENIED) {
+                alert("Please grant Location permission!");
+            } else {
+                alert("Error,  reset location permission.");
+            }
+        });
+}
+
+locationButton.addEventListener("click", getUserCoordinates);
+searchButton.addEventListener("click", getCityCoordinates);
+cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
